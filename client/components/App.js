d95cdc44d8649a9c062f3c2f494e185dfc1d139a
Thomas Weidner <tomweidner103@gmail.com>
2019-11-13 08:00:45 -0800

    set up routes, navlink

--- client/components/App.js
+++ client/components/App.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Nav from './Nav'
+import Routes from './Routes'
 
 class App extends React.Component {
     render(){
         return(
-            <div>Hello World</div>
+            <div>
+                <Nav />
+                <Routes />
+            </div>
         )
     }
 }
