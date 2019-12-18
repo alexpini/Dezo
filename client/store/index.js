70ad1831dfc7102e1c55a65d7a935e6ee169b326
tomweidner103 <tomweidner103@gmail.com>
2019-12-18 10:56:36 -0800

    Update index.js

--- client/store/index.js
+++ client/store/index.js
@@ -1,11 +1,10 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux'
 import thunkMiddleware from 'redux-thunk'
-
-const user = (state=[], action) => async dispatch => {
-    return state
-}
+import user from './user
 
 const reducer = combineReducers({user})
 const store = createStore(reducer, applyMiddleware(thunkMiddleware))
 
 export default store
+
+export * from './user'
