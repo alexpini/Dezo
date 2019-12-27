3930eb1548fa3a36fa97b3c7947fe1f2e59f15af
tomweidner103 <tomweidner103@gmail.com>
2019-12-27 13:50:00 -0800

    Update TwentyOne.js

--- client/components/Modals/TwentyOne.js
+++ client/components/Modals/TwentyOne.js
@@ -4,39 +4,6 @@ export const TwentyOne = props => {
   return (
     <div>
       <section className="modal-main modal-top">
- master
-        <section className="modal-bottom">
-          <h1>Are you of legal drinking age?</h1>
-          <section style={{ display: "flex", justifyContent: "space-around" }}>
-            <div
-              onClick={e => props.submitAge(e, true)}
-              style={{
-                display: "flex",
-                width: "4rem",
-                height: "4rem",
-                backgroundColor: "red",
-                flexDirection: "column",
-                justifyContent: "center"
-              }}
-            >
-              Yes
-            </div>
-            <div
-              onClick={e => props.submitAge(e, false)}
-              style={{
-                display: "flex",
-                width: "4rem",
-                height: "4rem",
-                backgroundColor: "red",
-                flexDirection: "column",
-                justifyContent: "center"
-              }}
-            >
-              No
-            </div>
-          </section>
-        </section>
-
         <h2>Respectfully Wild</h2>
         <div className="modal-bottom">
 
@@ -68,7 +35,6 @@ export const TwentyOne = props => {
           <p id='enter'>You must be 21 or older to enter this website</p>
           <span>{props.error}</span>
         </div>
- dev
       </section>
     </div>
   );
