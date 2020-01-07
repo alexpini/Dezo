bd5e6d238babba604d6e9a9986ee2319ab96ed83
Thomas Weidner <tomweidner103@gmail.com>
2020-01-06 17:26:59 -0800

    fix styles

--- client/components/Modals/TwentyOne.js
+++ client/components/Modals/TwentyOne.js
@@ -31,7 +31,7 @@ export const TwentyOne = props => {
             />
             <button type="submit">ENTER</button>
           </form>
-          <p id="enter">You must be 21 or older to enter this website</p>
+          <div id="enter">You must be 21 or older to enter this website</div>
           <span>{props.error}</span>
         </div>
       </section>
