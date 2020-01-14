6b79f3086b2f820a621f415c36bc64ed8a34b938
Thomas Weidner <tomweidner103@gmail.com>
2020-01-14 00:26:59 -0800

    force true

--- server/index.js
+++ server/index.js
@@ -132,7 +132,8 @@ const syncDb = async () => {
     },
     defaults: {
       description: "Small blurb about the article goes here.",
-      imgURL: "https://cdn.artaic.com/wp-content/uploads/2015/03/artaic-orange-nature-poppy-tangerine-splash-tile-0300810-900x600.jpg",
+      imgURL:
+        "https://cdn.artaic.com/wp-content/uploads/2015/03/artaic-orange-nature-poppy-tangerine-splash-tile-0300810-900x600.jpg",
       link: "https://www.instagram.com/drinkdezo/"
     }
   });
@@ -156,8 +157,8 @@ const syncDb = async () => {
       link: "https://www.instagram.com/drinkdezo/"
     }
   });
-  // db.sync({ force: true });
-  db.sync();
+  db.sync({ force: true });
+  // db.sync();
 };
 
 async function bootApp() {
