58e54b240f8f8abefc4d792499550d6a34c1ef29
Alexandra Pini <alexandrampini@gmail.com>
2020-01-13 23:07:04 -0500

    scroll UI

--- client/components/About/Page3.js
+++ client/components/About/Page3.js
@@ -2,18 +2,16 @@ import React from "react";
 
 export const Page3 = props => {
   return (
-    <section className="about-page_3">
+    <section id="about" className="about-page_3">
       <div className="about-page_3-box">
         <div className="row">
         <div className="col-xs-6">
           <h1>What Is Dezo?</h1>
           </div>
           <div className="col-xs-6">
-          <div className="typewriter">
             <p>
-              Dezo is derived from the French phrase "Des Eaux."
+              Dezo is derived from the French phrase "Des Eaux" meaning "Of the Waters".
             </p>
-          </div>
           </div>
         </div>
       </div>
