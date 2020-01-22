866268437a4778b99f1207d5165c6502097bfff5
Alexandra Pini <alexandrampini@gmail.com>
2020-01-22 03:25:32 -0500

    what-is-dezo color

--- client/components/About/Page3.js
+++ client/components/About/Page3.js
@@ -6,7 +6,7 @@ export const Page3 = props => {
       <div className="about-page_3-box">
         <div className="row">
         <div className="col-xs-6">
-          <h1>What Is Dezo?</h1>
+          <h1 id="what-is-dezo">What Is Dezo?</h1>
           </div>
           <div className="col-xs-6">
             <p>
